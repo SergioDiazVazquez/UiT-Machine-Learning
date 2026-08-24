14cce33bbf4765476783c4cc4523d83d1d06e2e6
SergioDiazVazquez <sergiodiavaz@gmail.com>
2026-08-24 13:04:03 +0200

    problem 2a

---
+++ exercise-2/problem-2/problem-2a.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# 1. Load the data
+df = pd.read_csv("exercise-2/global-temperatures.csv", sep=r"\s+")
+
+X = df[["Year"]]
+y = df["Temperature"]
+
+# 2. Model
+model = LinearRegression()
+
+# 3. Train it on the dataset
+model.fit(X,y)
+
+# 4. Generate the predicted temperatures
+y_pred = model.predict(X)
+
+# 5. Plot the data
+
+# 1. Plot the actual data points
+plt.scatter(X, y, color='blue', label='Actual Temperatures')
+
+# 2. Plot the regression line
+plt.plot(X, y_pred, color='red', linewidth=2, label='Regression Line')
+
+# 3. Add labels and a legend
+plt.xlabel('Year')
+plt.ylabel('Temperature')
+plt.title('Temperature vs. Years')
+plt.legend()
+
+# 4. Show the graph
+plt.show()
